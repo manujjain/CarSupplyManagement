e46b79d2eefa8fb3d155d126930993e898b34bea
Manuj jain <32789683+manujjain@users.noreply.github.com>
2018-02-01 11:02:05 -0500

    Update wsgi.py

--- supplychain/supplychain/wsgi.py
+++ supplychain/supplychain/wsgi.py
@@ -10,7 +10,15 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import os
+
+from whitenoise.django import DjangoWhiteNoise
+
+
+
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "supplychain.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
